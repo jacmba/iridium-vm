ad57f98921110d23df11dbda421f2d8d7b992b39
Jacinto Mba Cantero <jacmba@gmail.com>
2022-06-01 19:49:56 +0100

    Integer operand parsing

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,10 +1,12 @@
 use super::instruction::Opcode;
 
 pub mod opcode_parser;
+pub mod operand_parser;
 pub mod register_parser;
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
   Op { code: Opcode },
   Register { reg_num: u8 },
+  IntegerOperand { value: i32 },
 }
---
+++ src/assembler/operand_parser.rs
@@ -0,0 +1,38 @@
+use nom::digit;
+use nom::types::CompleteStr;
+
+use crate::assembler::Token;
+
+named!(integer_operand<CompleteStr, Token>,
+  ws!(
+    do_parse!(
+      tag!("#") >>
+      reg_num: digit >>
+      (
+        Token::IntegerOperand {value: reg_num.parse::<i32>().unwrap()}
+      )
+    )
+  )
+);
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_parse_integer_operand() {
+    let result = integer_operand(CompleteStr("#10"));
+    assert!(result.is_ok());
+    let (rest, value) = result.unwrap();
+    assert_eq!(rest, CompleteStr(""));
+    assert_eq!(value, Token::IntegerOperand { value: 10 });
+  }
+
+  #[test]
+  fn test_parse_invalid_integer_operands() {
+    let result = integer_operand(CompleteStr("10"));
+    assert!(!result.is_ok());
+
+    let result = integer_operand(CompleteStr("#a"));
+    assert!(!result.is_ok());
+  }
+}
